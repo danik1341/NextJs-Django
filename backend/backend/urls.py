fe52c49281d583c840057f4517b8193e83376230
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-08-23 18:06:51 +0300

    backend init setup

---
+++ backend/backend/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from reviews import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'reviews', views.ReviewViewSet)
+router.register(r'businesses', views.BusinessViewSet)
+router.register(r'categories', views.CategoryViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
