6cfaaf5476eea5effb6b99c3141f83b5198f2ba3
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-08-29 14:50:25 +0300

    categories data fech and page

--- frontend/app/categories/[id]/page.tsx
+++ frontend/app/categories/[id]/page.tsx
@@ -1,7 +1,125 @@
-import React from 'react';
+"use client";
 
-const Category:React.FC = () => {
-    
-    return <div>Category</div>
-}
+import { Box, Card, CardContent, Grid, Link, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+type Business = {
+  url: string;
+  name: string;
+  description: string;
+  price_range: string;
+  street_address: string;
+  city: string;
+  region: string;
+  postal_code: string;
+  country: string;
+  phone: string;
+  hours: string;
+  id: number;
+};
+
+type Category = {
+  url: string;
+  name: string;
+  ordinal: number;
+  business: Business[];
+  id: number;
+};
+
+type CategoryDetailsProps = {
+  params: { id: string };
+};
+
+const fetchCategory = async (id: string) => {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}/`
+    );
+    return response.data;
+  } catch (err) {
+    console.error("Error fetching category data:", err);
+    throw err;
+  }
+};
+
+const Category: React.FC<CategoryDetailsProps> = ({ params }) => {
+  const id = params.id;
+  const [category, setCategory] = useState<Category>();
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchCategory(id);
+        setCategory(data);
+      } catch (error) {
+        console.error("Error fetching category data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const handleBusinessClick = (id: number) => {
+    router.push(`/business/${id}`);
+  };
+
+  return (
+    <Grid container className=" mt-6 max-w-[95vw]">
+      <Grid item xs={12} md={3}>
+        todo filters
+      </Grid>
+
+      <Grid item xs={12} md={9} className=" space-y-5">
+        {category?.business.map((business, index) => (
+          <Card
+            className=" cursor-pointer"
+            key={index}
+            onClick={() => handleBusinessClick(business.id)}
+          >
+            <Box>
+              <CardContent>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography variant="h5">{business.name}</Typography>
+                    <Typography variant="subtitle1">
+                      {business.price_range}
+                    </Typography>
+                    <Link
+                      variant="subtitle1"
+                      href={business.url}
+                      target="_blank"
+                    >
+                      Business Website
+                    </Link>
+                    <Typography variant="subtitle1">
+                      {business.phone}
+                    </Typography>
+                    <Typography variant="subtitle1" className=" text-gray-500">
+                      {business.description}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Typography variant="h5">Todo Reviews</Typography>
+                    <Typography variant="subtitle1">
+                      {business.hours}
+                    </Typography>
+                    <Typography variant="subtitle1">
+                      {business.street_address} {business.city},{" "}
+                      {business.region} {business.postal_code}{" "}
+                      {business.country}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Box>
+          </Card>
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
 export default Category;
