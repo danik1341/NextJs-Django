199bff701347421f49042669c81c0072baefa07a
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-09-03 10:28:26 +0300

    backend settings and url finished phase1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "COERCE_DECIMAL_TO_STRING": False,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 INSTALLED_APPS = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,16 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from reviews import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'reviews', views.ReviewViewSet)
-router.register(r'businesses', views.BusinessViewSet)
-router.register(r'categories', views.CategoryViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+router.register(r"reviews", views.ReviewViewSet)
+router.register(r"businesses", views.BusinessViewSet)
+router.register(r"categories", views.CategoryViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/user/", views.UserAPIView.as_view(), name="login"),
+    path("api/register/", views.RegisterUserAPIView.as_view(), name="register"),
 ]
